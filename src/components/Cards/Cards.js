1eeb7b0b05ed95f4b3f4703f719b9b7e13d62c84
bipon68 <bipon68@users.noreply.github.com>
2020-08-01 10:10:14 +0600

    countup into component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -16,7 +16,15 @@ const Cards = ({data: {confirmed,recovered,deaths,lastUpdate}}) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary">Infected</Typography>
-                        <Typography variant="h5">{confirmed.value}</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        
+                        </Typography>
                         <Typography color="textSecondary">Real Date</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
