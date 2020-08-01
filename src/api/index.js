b38c24ff0b9f919328b615c73a59b5ddb8ce36d9
bipon68 <bipon68@users.noreply.github.com>
2020-08-01 08:25:16 +0600

    response data destructing

--- src/api/index.js
+++ src/api/index.js
@@ -5,16 +5,17 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
 
     try{
-        const {data} = await axios.get(url);
+        const {data: {confirmed,recovered,deaths,lastUpdate }} = await axios.get(url);
 
-        const modifiedData = {
-            confirmed: data.confirmed,
-            recovered: data.recovered,
-            deaths: data.deaths,
-            lastUpdate: data.lastUpdate
-        }
+        // const modifiedData = {
+        //     confirmed,
+        //     recovered,
+        //     deaths,
+        //     lastUpdate
+        // }
 
-        return modifiedData;
+        return {confirmed, recovered, deaths, lastUpdate};
+        // return modifiedData;
 
     }catch(error){
 
