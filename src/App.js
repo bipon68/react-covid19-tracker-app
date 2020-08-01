899e0d8ced7ec040eda3b89dbc11f729f0b891f0
bipon68 <bipon68@users.noreply.github.com>
2020-08-01 07:56:37 +0600

    styles module added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './App.css';
 
 // import Cards from './components/Cards/Cards';
 // import Charts from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
 
 import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.container}>
       <Cards />
       <Chart />
       <CountryPicker />
